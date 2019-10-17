f17b512c3c72e2c665c264ad0842c83225657b10
t-watanabe-r <takahiro.watanabe1@jp.ricoh.com>
2019-10-18 08:56:01 +0900

    Fix folder

--- dc/dc1_1/DegitalClock.java
+++ dc/dc1_1/DegitalClock.java
@@ -1,7 +1,12 @@
-package dc;
+package dc1_1;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Calendar;
 
 /**
@@ -9,15 +14,15 @@ import java.util.Calendar;
  *
  */
 public class DegitalClock extends Frame implements Runnable{
-	
-	final static int WINDOW_WIDTH = 300;
+
+    final static int WINDOW_WIDTH = 300;
 	final static int WINDOW_HIGHT = 200;
-	
+
 	final static int CLOCK_WIDTH = 200;
 	final static int CLOCK_HIGHT = 200;
 	final static int CLOCK_CHAR_NUM = 8;
-	
-	
+
+
 	static int hour;
 	static int minute;
 	static int second;
@@ -26,7 +31,7 @@ public class DegitalClock extends Frame implements Runnable{
 	static DegitalClock clock = new DegitalClock();
 	static Thread th = new Thread(clock);
 	Calendar now = Calendar.getInstance();
-	
+
     public void run(){
         while(true){
               hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
@@ -35,20 +40,20 @@ public class DegitalClock extends Frame implements Runnable{
               repaint();
 
               try{
-                  Thread.sleep(1000);  //ÉXÉäÅ[ÉvÇPïb
+                  Thread.sleep(1000);
               }catch(InterruptedException e){
               }
         }
     }
-    
+
     public void paint(Graphics g)
     {
     	Dimension size = getSize();
-    	
+
         String time = String.format("%02d", hour) + ":"
 					+ String.format("%02d", minute) + ":"
 					+ String.format("%02d", second);
-        
+
         int fontSize = size.width / CLOCK_CHAR_NUM; // windowSize / charNum
         Font font = new Font(Font.DIALOG_INPUT, Font.BOLD, fontSize);
         g.setFont(font);
@@ -56,14 +61,14 @@ public class DegitalClock extends Frame implements Runnable{
         g.drawString(time, (size.width - (fontSize * CLOCK_CHAR_NUM / 2)) / 2, (size.height + fontSize) / 2);
 
     }
-    
+
     public static void main(String args[]) {
-    	
+
     	// Initialize window
     	clock.setSize(WINDOW_WIDTH, WINDOW_HIGHT);
     	clock.setVisible(true);
     	clock.addWindowListener(new Ada());
-    	
+
     	th.start();
     }
 
